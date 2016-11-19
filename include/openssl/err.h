6c6f54af31d0809d0810cda8afc6c7f5dcddfa0a
Richard Levitte <levitte@openssl.org>
2016-11-19 11:17:44 +0100

    Add a URI decoder
    This is a needed component for a URI based STORE module.
    
    This includes a perl script crypto/uri_charmap.pl that generates a
    local header file to codify the URI character classes.  See RFC3986
    for further reference.

--- include/openssl/err.h
+++ include/openssl/err.h
@@ -93,6 +93,7 @@ typedef struct err_state_st {
 # define ERR_LIB_CT              50
 # define ERR_LIB_ASYNC           51
 # define ERR_LIB_KDF             52
+# define ERR_LIB_URI             53
 
 # define ERR_LIB_USER            128
 
@@ -131,6 +132,7 @@ typedef struct err_state_st {
 # define CTerr(f,r) ERR_PUT_error(ERR_LIB_CT,(f),(r),OPENSSL_FILE,OPENSSL_LINE)
 # define ASYNCerr(f,r) ERR_PUT_error(ERR_LIB_ASYNC,(f),(r),OPENSSL_FILE,OPENSSL_LINE)
 # define KDFerr(f,r) ERR_PUT_error(ERR_LIB_KDF,(f),(r),OPENSSL_FILE,OPENSSL_LINE)
+# define URIerr(f,r) ERR_PUT_error(ERR_LIB_URI,(f),(r),OPENSSL_FILE,OPENSSL_LINE)
 
 # define ERR_PACK(l,f,r) ( \
         (((unsigned int)(l) & 0x0FF) << 24L) | \
