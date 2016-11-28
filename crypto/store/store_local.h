98045d23927db227cb96e647ce9449ffae15ef0d
Richard Levitte <levitte@openssl.org>
2016-11-28 17:24:26 +0100

    Add the STORE module
    This STORE module adds the following functionality:
    
    - A function STORE_open(), STORE_load() and STORE_close() that accesses
      a URI and helps loading the supported objects (PKEYs, CERTs and CRLs
      for the moment) from it.
    - An opaque type STORE_INFO that holds information on each loaded
      object.
    - A few functions to retrieve desired data from a STORE_INFO
      reference.
    - Functions to register and unregister loaders for different URI
      schemes.  This enables dynamic addition of loaders from applications
      or from engines.

---
+++ crypto/store/store_local.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 1995-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include "internal/thread_once.h"
+#include <openssl/engine.h>
+#include <openssl/evp.h>
+#include <openssl/lhash.h>
+#include <openssl/x509.h>
+#include <openssl/store.h>
+
+struct store_info_st {
+    int type;
+    union {
+        char *name;              /* when type == STORE_INFO_NAME */
+        EVP_PKEY *pkey;          /* when type == STORE_INFO_PKEY */
+        X509 *x509;              /* when type == STORE_INFO_X509 */
+        X509_CRL *crl;           /* when type == STORE_INFO_CRL */
+        void *data;              /* used internally */
+    } _;
+};
+
+int store_init_once(void);
+int store_register_loader_int(STORE_LOADER *loader);
+STORE_LOADER *store_unregister_loader_int(const char *scheme);
+
+/* loader stuff */
+typedef struct store_loader_st {
+    const char *scheme;
+    STORE_open_fn open;
+    STORE_load_fn load;
+    STORE_eof_fn eof;
+    STORE_close_fn close;
+} STORE_LOADER;
+DEFINE_LHASH_OF(STORE_LOADER);
+
+const STORE_LOADER *store_get0_loader_int(const char *scheme);
+void destroy_loaders_int(void);
