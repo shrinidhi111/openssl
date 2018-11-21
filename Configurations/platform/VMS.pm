ecc03c9eb642df59b56708374c2842780af28edf
Richard Levitte <levitte@openssl.org>
2018-11-21 16:39:17 +0100

    Rework building: VMS changes to handle installation
    Add installation functions to platform::VMS.
    
    This also reworks Configurations/descrip.mms.tmpl to use the
    platform::VMS functionality for all installations, except
    documentation, which works more or less uniformly everywhere.

--- Configurations/platform/VMS.pm
+++ Configurations/platform/VMS.pm
@@ -61,4 +61,122 @@ sub sharedname {
                                     "_shr$target{pointer_size}");
 }
 
+sub _install_file {
+    my $srcpath = shift;
+    my $dstpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+
+    return <<"_____";
+${at}COPY/PROT=W:R $srcpath $dstpath
+_____
+}
+
+sub install_file {
+    my $self = shift;
+    my $srcpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+
+    my $srcfile = basename($srcpath);
+    my $dstdir = platform->get_install_path($opts{type});
+    my $dstpath = File::Spec->catfile($dstdir, $opts{dstfile} // $srcfile);
+
+    if ($opts{type} eq 'conf') {
+        (my $cmds1 = _install_file($srcpath, "$dstpath-dist", %opts))
+            =~ s/\n$//;
+        my $cmds2 = join("\n", map { "\t$_; \\" } split /\n/,
+                         _install_file($srcpath, $dstpath, %opts, silent => 0));
+        return split /\n/, <<"_____";
+$cmds1
+${at}IF F\$SEARCH("$dstpath") .EQS. "" THEN -
+$cmds2
+fi
+_____
+    }
+
+    return  split /\n/, _install_file($srcpath, $dstpath, %opts);
+}
+
+sub uninstall_file {
+    my $self = shift;
+    my $srcpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+
+    my $srcfile = basename($srcpath);
+    my $dstdir = platform->get_install_path($opts{type});
+    my $dstpath = File::Spec->catdir($dstdir, $opts{dstfile} // $srcfile).';*';
+
+    return split /\n/, <<"_____";
+${at}\$(RM) $dstpath
+_____
+}
+
+sub install_exec {
+    my $self = shift;
+    my $srcpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@' : '';
+
+    my ($srcvol, $srcdir, $srcfile) = File::Spec->splitpath($srcpath);
+    my $dstdir = platform->get_install_path($opts{type});
+    my $dstpath = File::Spec->catpath('', $dstdir, $opts{dstfile} // $srcfile);
+
+    return split /\n/, <<"_____";
+${at}COPY/PROT=W:RE $srcpath $dstpath
+_____
+}
+
+sub uninstall_exec {
+    goto &uninstall_file;
+}
+
+use File::Basename;
+
+use if $^O eq 'VMS', 'VMS::Filespec' ; # To get fileify()
+# to simulate fileify, which becomes an identity functions on other OSes
+*fileify = sub { $_[0]; } unless $^O eq 'VMS';
+
+sub install_dir {
+    my $self = shift;
+    my %opts = @_;
+
+    my $at = $opts{silent} ? '@ ' : '';
+    my $type = {
+        $opts{type}     => $opts{type},
+        runtimelib      => 'lib',
+        devlib          => 'lib',
+    } -> {$opts{type}};
+    my $dir = platform->get_install_path($type);
+    my $dirfile = fileify($dir);
+    my $permission = {
+        !1              => 'S:RWED,O:RWE,G:RE,W:RE',
+        !!1             => 'S:RWED,O:RWE,G,W',
+    } -> {!!$opts{private}};
+
+    return split /\n/, <<"_____"
+${at}IF F\$SEARCH("$dirfile") .EQS. "" THEN -
+        CREATE/DIR/PROT=($permission) $dir
+_____
+}
+
+sub uninstall_dir {
+    my $self = shift;
+    my %opts = @_;
+
+    my $at = $opts{silent} ? '@ ' : '';
+    my $type = {
+        $opts{type}     => $opts{type},
+        runtimelib      => 'lib',
+        devlib          => 'lib',
+    } -> {$opts{type}};
+    my $dir = platform->get_install_path($type);
+    my $dirfile = fileify($dir);
+
+    return split /\n/, <<"_____";
+-${at}DELETE $dirfile
+_____
+}
+
 1;
