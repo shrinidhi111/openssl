08995ad852f21c05bb6e0a568d9c9fac487479c6
Richard Levitte <levitte@openssl.org>
2018-11-21 16:37:41 +0100

    Rework building: Windows changes to handle installation
    Add installation functions to platform::Windows.
    
    This also reworks Configurations/windows-makefile.tmpl to use the
    platform::Windows functionality for all installations, except
    documentation, which works more or less uniformly everywhere.

--- Configurations/platform/Windows.pm
+++ Configurations/platform/Windows.pm
@@ -60,4 +60,132 @@ sub sharedlib_import {
                                     $_[0]->shlibextimport());
 }
 
+sub _install_file {
+    my $srcpath = shift;
+    my $dstpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+    my $copyargs = $opts{copyargs} // '';
+
+    return <<"_____";
+${at}\$(ECHO) "install $srcpath -> $dstpath"
+${at}"\$(PERL)" "\$(SRCDIR)\\util\\copy.pl" $copyargs "$srcpath" "$dstpath"
+_____
+}
+
+use File::Basename;
+
+sub install_file {
+    my $self = shift;
+    my $srcpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+
+    my $srcfile = basename($srcpath);
+    my $dstdir = platform->get_install_path($opts{type});
+    my $dstpath = File::Spec->catfile($dstdir, $opts{dstfile} // $srcfile);
+
+    if ($opts{type} eq 'conf') {
+        (my $cmds1 = _install_file($srcpath, "$dstpath-dist", %opts))
+            =~ s/\n$//;
+        my $cmds2 = join("\n", map { "\t$_; \\" } split /\n/,
+                         _install_file($srcpath, $dstpath, %opts, silent => 0));
+        return split /\n/, <<"_____";
+$cmds1
+${at}IF NOT EXIST "$dstpath" THEN \\
+$cmds2
+fi
+_____
+    }
+
+    return  split /\n/, _install_file($srcpath, $dstpath, %opts);
+}
+
+sub uninstall_file {
+    my $self = shift;
+    my $srcpath = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@ ' : '';
+
+    my $srcfile = basename($srcpath);
+    my $dstdir = platform->get_install_path($opts{type});
+    my $dstpath = File::Spec->catdir($dstdir, $opts{dstfile} // $srcfile);
+
+    return split /\n/, <<"_____";
+${at}\$(ECHO) "\$(RM) $dstpath"
+${at}\$(RM) $dstpath
+_____
+}
+
+sub install_exec {
+    goto &install_file;
+}
+
+sub uninstall_exec {
+    goto &install_file;
+}
+
+sub install_dir {
+    my $self = shift;
+    my %opts = @_;
+
+    my $at = $opts{silent} ? '@ ' : '';
+    my $type = {
+        $opts{type}     => $opts{type},
+        runtimelib      => 'bin',
+        devlib          => 'lib',
+    } -> {$opts{type}};
+    my $dir = platform->get_install_path($type);
+
+    return split /\n/, <<"_____"
+${at}"\$(PERL)" "\$(SRCDIR)\\util\\mkdir-p.pl" "$dir"
+_____
+}
+
+sub uninstall_dir {
+    my $self = shift;
+    my %opts = @_;
+
+    my $at = $opts{silent} ? '@ ' : '';
+    my $type = {
+        $opts{type}     => $opts{type},
+        runtimelib      => 'bin',
+        devlib          => 'lib',
+    } -> {$opts{type}};
+    my $dir = platform->get_install_path($type);
+
+    return split /\n/, <<"_____";
+-${at}\$(RMDIR) "$dir"
+_____
+}
+
+# Shared libraries are different on Windows
+sub install_lib {
+    my $self = shift;
+    my $libname = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@' : '';
+
+    my $shared_lib = platform->sharedlib($libname);
+
+    if ($opts{type} eq 'devlib') {
+        # Install a static library as a normal file
+        my @cmds = platform->install_file(platform->staticlib($libname),
+                                          %opts, type => 'lib');
+
+        # Also install the import library as a normal file
+        push @cmds,
+            platform->install_file(platform->sharedlib_import($libname),
+                                   %opts, type => 'lib')
+            if $shared_lib;
+
+        return @cmds;
+    }
+
+    return platform->install_exec($shared_lib, %opts, type => 'bin')
+        if $opts{type} eq 'runtimelib' && $shared_lib;
+
+    return platform->SUPER::install_lib($libname, %opts);
+}
+
 1;
--- Configurations/platform/Windows/MSVC.pm
+++ Configurations/platform/Windows/MSVC.pm
@@ -30,4 +30,59 @@ sub binpdb {
     return platform::BASE::__concat($_[0]->binname($_[1]), $_[0]->pdbext());
 }
 
+sub install_lib {
+    my $self = shift;
+    my $libname = shift;
+    my %opts = @_;
+    my $at = $opts{silent} ? '@' : '';
+
+    my $shared_lib = platform->sharedlib($libname);
+    my @extra = ();
+
+    push @extra,
+        platform->install_file(platform->staticlibpdb($libname).
+                               %opts, type => 'lib')
+        if $opts{type} eq 'devlib' && !$shared_lib;
+
+    push @extra,
+        platform->install_exec(platform->sharedlibpdb($libname),
+                                    %opts, type => 'bin')
+        if $opts{type} eq 'runtimelib' && $shared_lib;
+
+    return ( platform->SUPER::install_lib($libname, %opts),
+             @extra );
+}
+
+sub install_dso {
+    my $self = shift;
+    my $dsoname = shift;
+
+    return ( platform->SUPER::install_dso($dsoname, @_),
+             platform->install_file(platform->dsopdb($dsoname), @_) );
+}
+
+sub uninstall_dso {
+    my $self = shift;
+    my $dsoname = shift;
+
+    return ( platform->SUPER::uninstall_dso($dsoname, @_),
+             platform->uninstall_file(platform->dsopdb($dsoname), @_) );
+}
+
+sub install_bin {
+    my $self = shift;
+    my $binname = shift;
+
+    return ( platform->SUPER::install_bin($binname, @_),
+             platform->install_file(platform->binpdb($binname), @_) );
+}
+
+sub uninstall_bin {
+    my $self = shift;
+    my $binname = shift;
+
+    return ( platform->SUPER::uninstall_bin($binname, @_),
+             platform->uninstall_file(platform->binpdb($binname), @_) );
+}
+
 1;
