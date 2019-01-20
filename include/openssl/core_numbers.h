e426ce0fa4c7a4348b32d701956dd2fba6e08820
Richard Levitte <levitte@openssl.org>
2019-01-20 14:09:46 +0100

    Replumbing: Add future provider functionality (for backup)

--- include/openssl/core_numbers.h
+++ include/openssl/core_numbers.h
@@ -72,6 +72,39 @@ OSSL_CORE_MAKE_FUNC(const OSSL_ALGORITHM *,provider_query_operation,
                     (const OSSL_PROVIDER *, int operation_id,
                      const int *no_store))
 
+/*
+ * Operation identities, used with the provider query_operation function to
+ * get the array of algorithm implementations for that operation.
+ */
+#define OSSL_OP_DIGEST                         1
+#define OSSL_OP_SYM_ENCRYPT                    2
+#define OSSL_OP_SEAL                           3
+#define OSSL_OP_DIGEST_SIGN                    4
+#define OSSL_OP_SIGN                           5
+#define OSSL_OP_ASYM_KEYGEN                    6
+#define OSSL_OP_ASYM_PARAMGEN                  7
+#define OSSL_OP_ASYM_ENCRYPT                   8
+#define OSSL_OP_ASYM_SIGN                      9
+#define OSSL_OP_ASYM_DERIVE                   10
+
+/*
+ * Operation function identities, for the OSSL_DISPATCH tables wrapped
+ * in OSSL_ALGORITHM
+ * The names of the identity macros are slightly different than the Core
+ * identities, by having _FUNC last.
+ */
+#define OSSL_OP_DIGEST_NEWCTX_FUNC          1536
+OSSL_CORE_MAKE_FUNC(void *,OP_digest_newctx,(void))
+#define OSSL_OP_DIGEST_INIT_FUNC            1537
+OSSL_CORE_MAKE_FUNC(int,OP_digest_init,(/* FIXME: To be determined */void))
+#define OSSL_OP_DIGEST_UPDATE_FUNC          1538
+OSSL_CORE_MAKE_FUNC(int,OP_digest_update,(/* FIXME: To be determined */void))
+#define OSSL_OP_DIGEST_FINAL_FUNC           1539
+OSSL_CORE_MAKE_FUNC(int,OP_digest_final,(/* FIXME: To be determined */void))
+#define OSSL_OP_DIGEST_CLEANCTX_FUNC        1540
+OSSL_CORE_MAKE_FUNC(void,OP_digest_cleanctx,(void *))
+#define OSSL_OP_DIGEST_FREECTX_FUNC         1541
+OSSL_CORE_MAKE_FUNC(void,OP_digest_freectx,(void *))
 
 # ifdef __cplusplus
 }
